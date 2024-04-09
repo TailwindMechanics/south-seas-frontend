fe9e386cfc18f2af56da083523129f2d7bc267cb
Tailwind Mechanics <shea@tailwindmechanics.com>
2024-04-09 15:24:20 +0100

    Login status message now auto hides

--- src/components/ui/Login.tsx
+++ src/components/ui/Login.tsx
@@ -8,11 +8,12 @@ import { SupabaseClient } from "../../utils/SupabaseClient";
 import { Button } from "./Button";
 
 export const Login = () => {
+  const [confirmPassword, setConfirmPassword] = useState<string>("");
+  const [statusMessage, setStatusMessage] = useState<string>("");
+  const [selectedIndex, setSelectedIndex] = useState(0);
+  const [password, setPassword] = useState<string>("");
   const [user, setUser] = useState<User | null>(null);
   const [email, setEmail] = useState<string>("");
-  const [password, setPassword] = useState<string>("");
-  const [confirmPassword, setConfirmPassword] = useState<string>("");
-  const [selectedIndex, setSelectedIndex] = useState(0);
 
   async function onLoginSubmit(event: React.FormEvent<HTMLFormElement>) {
     console.log("onLoginSubmit");
@@ -26,7 +27,7 @@ export const Login = () => {
     });
 
     if (response.error) {
-      console.error("Error signing in:", response.error.message);
+      SetStatusAndClear(response.error.message);
     } else {
       console.log("User signed in:", response.data.user);
       setUser(response.data.user);
@@ -42,19 +43,26 @@ export const Login = () => {
     const response = await supabase.auth.signUp({ email, password });
 
     if (response.error) {
-      console.error("Error signing up:", response.error.message);
+      SetStatusAndClear(response.error.message);
     } else {
       console.log("User signed up:", response.data.user);
       setUser(response.data.user);
     }
   }
 
+  function SetStatusAndClear(message: string) {
+    setStatusMessage(message);
+    setTimeout(() => {
+      setStatusMessage("");
+    }, 3000);
+  }
+
   async function onLogoutClick() {
     const supabase = SupabaseClient;
-    const { error } = await supabase.auth.signOut();
+    const response = await supabase.auth.signOut();
 
-    if (error) {
-      console.error("Error signing out:", error.message);
+    if (response.error) {
+      SetStatusAndClear(response.error.message);
     } else {
       console.log("User signed out");
       setUser(null);
@@ -62,99 +70,104 @@ export const Login = () => {
   }
 
   return (
-    <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
-      {user ? (
-        <>
-          <Button className="w-full" onClick={onLogoutClick}>
-            Logout
-          </Button>
-          <span>Logged in as: {user.email}</span>
-        </>
-      ) : (
-        <div className="h-full w-full flex-col items-center justify-center">
-          <Tab.List className="flex w-full justify-around rounded-t-lg">
-            <Tab
-              className={`w-full cursor-pointer px-4 py-2 text-xl focus:outline-none ${
-                selectedIndex === 0
-                  ? "font-semibold text-white underline"
-                  : "text-gray-400"
-              }`}>
-              Sign In
-            </Tab>
-            <Tab
-              className={`w-full cursor-pointer px-4 py-2 text-xl focus:outline-none ${
-                selectedIndex === 1
-                  ? "font-semibold text-white underline"
-                  : "text-gray-400"
-              }`}>
-              Sign Up
-            </Tab>
-          </Tab.List>
-          <Tab.Panels className="flex w-full flex-grow flex-col rounded">
-            <Tab.Panel className="flex w-full items-center justify-center">
-              <form className="w-full" onSubmit={onLoginSubmit}>
-                <div className="flex w-full flex-col items-center space-y-2">
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    autoComplete="current-password"
-                    className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-                    className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  <Button type="submit" className="w-full">
-                    Login
-                  </Button>
-                  <a href="#" className="text-blue-500">
-                    Forgot password?
-                  </a>
-                </div>
-              </form>
-            </Tab.Panel>
-            <Tab.Panel className="flex w-full items-center justify-center">
-              <form className="w-full" onSubmit={onSignupSubmit}>
-                <div className="flex w-full flex-col items-center space-y-2">
-                  <input
-                    type="email"
-                    placeholder="Email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    autoComplete="current-password"
-                    className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  <input
-                    type="password"
-                    placeholder="Password"
-                    value={password}
-                    onChange={(e) => setPassword(e.target.value)}
-                    autoComplete="current-password"
-                    className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  <input
-                    type="password"
-                    placeholder="Confirm Password"
-                    value={confirmPassword}
-                    onChange={(e) => setConfirmPassword(e.target.value)}
-                    autoComplete="current-password"
-                    className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  />
-                  <Button type="submit" className="w-full">
-                    Signup
-                  </Button>
-                </div>
-              </form>
-            </Tab.Panel>
-          </Tab.Panels>
-        </div>
-      )}
-    </Tab.Group>
+    <>
+      <Tab.Group selectedIndex={selectedIndex} onChange={setSelectedIndex}>
+        {user ? (
+          <>
+            <Button className="w-full" onClick={onLogoutClick}>
+              Logout
+            </Button>
+            <span>Logged in as: {user.email}</span>
+          </>
+        ) : (
+          <div className="h-full w-full flex-col items-center justify-center">
+            <Tab.List className="flex w-full justify-around rounded-t-lg">
+              <Tab
+                className={`w-full cursor-pointer px-4 py-2 text-xl focus:outline-none ${
+                  selectedIndex === 0
+                    ? "font-semibold text-white underline"
+                    : "text-gray-400"
+                }`}>
+                Sign In
+              </Tab>
+              <Tab
+                className={`w-full cursor-pointer px-4 py-2 text-xl focus:outline-none ${
+                  selectedIndex === 1
+                    ? "font-semibold text-white underline"
+                    : "text-gray-400"
+                }`}>
+                Sign Up
+              </Tab>
+            </Tab.List>
+            <Tab.Panels className="flex w-full flex-grow flex-col rounded">
+              <Tab.Panel className="flex w-full items-center justify-center">
+                <form className="w-full" onSubmit={onLoginSubmit}>
+                  <div className="flex w-full flex-col items-center space-y-2">
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      autoComplete="current-password"
+                      className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                      className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <Button type="submit" className="w-full">
+                      Login
+                    </Button>
+                    <a href="#" className="text-blue-500">
+                      Forgot password?
+                    </a>
+                  </div>
+                </form>
+              </Tab.Panel>
+              <Tab.Panel className="flex w-full items-center justify-center">
+                <form className="w-full" onSubmit={onSignupSubmit}>
+                  <div className="flex w-full flex-col items-center space-y-2">
+                    <input
+                      type="email"
+                      placeholder="Email"
+                      value={email}
+                      onChange={(e) => setEmail(e.target.value)}
+                      autoComplete="current-password"
+                      className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                      type="password"
+                      placeholder="Password"
+                      value={password}
+                      onChange={(e) => setPassword(e.target.value)}
+                      autoComplete="current-password"
+                      className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <input
+                      type="password"
+                      placeholder="Confirm Password"
+                      value={confirmPassword}
+                      onChange={(e) => setConfirmPassword(e.target.value)}
+                      autoComplete="current-password"
+                      className="w-full rounded-lg border border-gray-300 p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                    />
+                    <Button type="submit" className="w-full">
+                      Signup
+                    </Button>
+                  </div>
+                </form>
+              </Tab.Panel>
+            </Tab.Panels>
+          </div>
+        )}
+      </Tab.Group>
+      <div className="text-center text-sm leading-none text-red-400">
+        {statusMessage}
+      </div>
+    </>
   );
 };

17bf8feefa4230b838d5a577fbfccc5db7e4728f
Tailwind Mechanics <shea@tailwindmechanics.com>
2024-04-11 14:03:40 +0100

    Added dotenv

--- src/utilities/SbClient.ts
+++ src/utilities/SbClient.ts
@@ -1,8 +1,9 @@
-//path: src\utils\SbClient.ts
-
 import { createClient } from "@supabase/supabase-js";
+import dotenv from "dotenv";
 
-export const SbClient = createClient(
-  import.meta.env.VITE_SUPABASE_URL as string,
-  import.meta.env.VITE_SUPABASE_KEY as string,
-);
+dotenv.config();
+
+const SUPABASE_URL = process.env.VITE_SUPABASE_URL as string;
+const SUPABASE_KEY = process.env.VITE_SUPABASE_KEY as string;
+
+export const SbClient = createClient(SUPABASE_URL, SUPABASE_KEY);
